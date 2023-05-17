725e1b4075f1d6867744f39f3729f44531ee89a0
rossiyareich <tawinjira@hotmail.com>
2023-05-18 00:54:10 +0700

    fixed issue with json array

--- src/utils/file_loader.py
+++ src/utils/file_loader.py
@@ -14,7 +14,7 @@ def load_config(filepath):
 
 def load_prompt_addition(filepath):
     with open(filepath, "r") as f:
-        raw = json.load(f)
+        raw = json.load(f)["prompt_addition"]
     prompt_additions = []
     for prompt_addition in raw:
         prompt_additions.append(prompt_addition["direction"])
